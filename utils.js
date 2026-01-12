3953bd7c52a874cd2b458d9cc0ff294dab56ac6e
developereleven11-create <developer.eleven11@gmail.com>
2026-01-12 15:44:13 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,1 +1,19 @@
+const fs = require("fs-extra");
+const path = require("path");
 
+function sanitizeFilename(url) {
+  return url
+    .replace(/^https?:\/\//, "")
+    .replace(/[^a-z0-9]/gi, "_")
+    .toLowerCase();
+}
+
+async function ensureDirs() {
+  await fs.ensureDir(path.join(__dirname, "screenshots/mobile"));
+  await fs.ensureDir(path.join(__dirname, "screenshots/desktop"));
+}
+
+module.exports = {
+  sanitizeFilename,
+  ensureDirs
+};
